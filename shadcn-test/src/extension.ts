830f48b795e6e65259249cc1682482415c4ce9aa
sohanemon <m.sohanemon@gmail.com>
2024-02-02 17:52:01 +0600

    feat(package): Add login script feat(snippets): Extend createContext fix(package): Update version number

--- shadcn-test/src/extension.ts
+++ shadcn-test/src/extension.ts
@@ -1,8 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
 import { ExtensionContext, commands, window } from 'vscode';
 import { showInputBox, showQuickPick } from './lib/basicInput';
 import { multiStepInput } from './lib/multiStepInput';
